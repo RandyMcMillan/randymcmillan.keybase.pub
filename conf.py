5a28d64c77f0bb7f18eb585fa1b216b9c78cb101
RandyMcMillan <randy.lee.mcmillan@gmail.com>
2020-11-08 22:05:01 -0500

    update from /Users/git/randymcmillan.keybase.pub on 1604891069

--- conf.py
+++ conf.py
@@ -81,7 +81,7 @@ author = u'@RandyMcMillan'
 # built documents.
 #
 # The short X.Y version.
-version = str(millis)+str('.')+str(block_time)
+version = str(block_time)+str('.')+str(millis)
 # The full version, including alpha/beta/rc tags.
 release = u''
 
