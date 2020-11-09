468c5bf18a5b6980df5902274a51c9ba58ffb147
RandyMcMillan <randy.lee.mcmillan@gmail.com>
2020-11-08 23:36:21 -0500

    update from randymcmillan.keybase.pub on 1604896549

--- conf.py
+++ conf.py
@@ -193,7 +193,7 @@ html_static_path = ['_static']
 # bottom, using the given strftime format.
 # The empty string is equivalent to '%b %d, %Y'.
 #
-html_last_updated_fmt = '%d%b%Y'+str('.')+str(version)
+html_last_updated_fmt = '%d%b%Y'+str(' (')+str(version)+str(')')
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
